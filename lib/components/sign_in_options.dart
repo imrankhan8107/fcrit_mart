3c906e1ea42b63462a371e36cc9ed583b48028fe
imrankhan8107 <imran.pathan.8202@gmail.com>
2022-01-19 09:39:36 +0530

    update

--- lib/components/sign_in_options.dart
+++ lib/components/sign_in_options.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class SigninOptions extends StatelessWidget {
   final IconData icon;
-  SigninOptions({required this.icon});
+  const SigninOptions({required this.icon});
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Card(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         child: Padding(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: Icon(
             icon,
             size: 50,
---
+++ lib/screens/sign_up_page.dart
@@ -0,0 +1,61 @@
+import 'package:fcrit_mart/components/text_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:fcrit_mart/components/appbar_button.dart';
+import 'package:fcrit_mart/components/bottom_button.dart';
+import 'package:fcrit_mart/components/sign_in_options.dart';
+import 'package:fcrit_mart/constants.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Appbarbutton(),
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Sign up with one of the following options.',
+                  style: kTextstyle,
+                ),
+                Row(
+                  children: const [
+                    SigninOptions(icon: Icons.g_mobiledata_outlined),
+                    SigninOptions(icon: Icons.g_mobiledata),
+                  ],
+                ),
+              ],
+            ),
+            const Credential(credentialname: 'Name'),
+            const Credential(credentialname: 'E-mail'),
+            const Credential(credentialname: 'Password'),
+            BottomButton(
+              buttontext: 'Sign-up',
+              text: 'Already have an account?',
+              textinbutton: 'Log-in',
+              onpress: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
